7ae98e6b2175b0ecbf7df1e51183658e3ce1c0ba
carleonis <carleonis@acer>
2022-01-18 12:56:18 -0300

    atualizaçãode acesso

--- business/Conexao.js
+++ business/Conexao.js
@@ -9,9 +9,7 @@ function conexao_bd(authorization) {
     console.log(token)
     //local
     return {"host": "localhost", "port": 3306, "user": "root", "pass": "mysql", "db": 'sag'};
-    //integrator
-    //const dados_bd = {"host": "localhost", "port": 3306, "user": "sag01", "pass": "PYAZ89ew", "db": 'sag01'};
-    //return dados_bd;
+
 }
 
 module.exports = {conexao_bd};
